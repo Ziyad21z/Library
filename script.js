c7b3d81b07dd4e9997f0a123a35bef379856a2e7
Ziyad <115634322+Ziyad21z@users.noreply.github.com>
2023-12-02 11:47:06 +0300

    12/2/2023 #2
    fixed some bugs and added the finished or not finished button

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 const Book = function (title, auther, pages, readingStatus) {
     this.title = title
     this.auther = auther
@@ -13,12 +12,15 @@ const Book = function (title, auther, pages, readingStatus) {
 function addBooks(title, auther, pages, readingStatus) {
     return new Book(title, auther, pages, readingStatus)
 }
-const s1 = addBooks('som1', 'sami', 2221, true)
+const s1 = addBooks('som1', 'sami', 2221, false)
 const s2 = addBooks('som2', 'sami', 2222, true)
 const s3 = addBooks('som3', 'sami', 2223, false)
 const s4 = addBooks('som3', 'sami', 2224, true)
+const s5 = addBooks('som3', 'sami', 2224, true)
+const s6 = addBooks('som3', 'sami', 2224, false)
+const s7 = addBooks('som3', 'sami', 2224, true)
 
-let myBooks = [s1, s2, s3, s4];
+let myBooks = [s1, s2, s3, s4, s5, s6, s7];
 myBooks.forEach((element) => {
     const cards = document.querySelector('.cards')
 
@@ -36,103 +38,32 @@ myBooks.forEach((element) => {
     pages.className = 'pages'
     pages.innerText = element.pages + ' pages'
 
-    const readingStatus = document.createElement('button')
-    readingStatus.className = element.readingStatus == true ? 'reading-status-true' : 'reading-status-false'
-    readingStatus.addEventListener('click', () => {
+    const readingStatusButton = document.createElement('button')
+    readingStatusButton.className = element.readingStatus === true ? 'reading-status-true' : 'reading-status-false'
+    readingStatusButton.innerText = element.readingStatus === true ? 'finished' : 'not finished'
+    readingStatusButton.addEventListener('click', () => {
         if (element.readingStatus) {
-            element.className = 'reading-status-false'
-        } else
-            element.className = 'reading-status-true'
+            element.readingStatus = false
+            readingStatusButton.className ='reading-status-false'
+            readingStatusButton.innerText ='not finished'
+        } else {
+            element.readingStatus = true
+            readingStatusButton.className ='reading-status-true'
+            readingStatusButton.innerText ='finished'
+        }
     })
 
     const remove = document.createElement('button')
     remove.className = 'remove-button'
+    remove.innerText = ' remove'
     remove.addEventListener('click', () => {
-        
+        // will do later :)
     })
-
 
     card.appendChild(title)
     card.appendChild(auther)
     card.appendChild(pages)
-    card.appendChild(readingStatus)
+    card.appendChild(readingStatusButton)
     card.appendChild(remove)
     cards.appendChild(card)
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const cards = document.querySelector('.cards')
-
-const card = document.createElement('div')
-card.className = 'card'
-
-const title = document.createElement('div')
-title.className = 'card-book-title'
-
-const auther =  document.createElement('div')
-
-const pages = document.createElement('div')
-pages.className = 'pages'
-
-const readingStatus = document.createElement('button')
-readingStatus.className = 'reading-status-button'
-
-const remove = document.createElement('button')
-remove.className = 'remove-button'
-
-card.appendChild(title)
-card.appendChild(auther)
-card.appendChild(pages)
-card.appendChild(readingStatus)
-card.appendChild(remove)
-cards.appendChild(card) */
-
-
-
-// myBooks.push(new Book(title, auther, pages, readingStatus))
-
-// function Book(title, auther, page, read) {
-//     this.title = title
-//     this.auther = auther
-//     this.page = page
-//     this.read = read
-//     this.printInfo = function () {
-//         return `the book ${this.title} by ${this.auther} has ${this.page} pages\nreading status: ${this.read}`
-//     };
-// }
-// function Person(name, salary) {
-//     this.name = name
-//     this.salary = salary
-// }
-// Person.prototype.info = function () {
-//     return(`${this.name} has ${this.salary}`);
-// }
-
-// function sami(name, salary, status) {
-//     Person.call(this, name, salary)
-//     this.status = status
-// }
-// sami.prototype.ismaried = function(s){
-//     return s;
-// }
-
-// Object.setPrototypeOf(sami.prototype, Person.prototype)
-
-// const smsm = new sami('sami', 99999999, false)
-// console.log(smsm.info());
