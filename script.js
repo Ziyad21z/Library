883aab63ce3ffeb16384c5a734358e14f46b10b3
Ziyad <115634322+Ziyad21z@users.noreply.github.com>
2023-12-02 10:07:26 +0300

    12/2/2023
    still need a box for the user to add his books and need to fix a bug where the reading status don't change but made some decent progress

--- script.js
+++ script.js
@@ -1,19 +1,64 @@
-let myBooks = [];
 
-let Book = function (title, readingStatus) {
+
+const Book = function (title, auther, pages, readingStatus) {
     this.title = title
+    this.auther = auther
+    this.pages = pages
     this.readingStatus = readingStatus
     let booksCount = 0;
     this.getBookCount = function () {
         return booksCount++;
     }
 }
-
-function addBooks(title, readingStatus) {
-Book.prototype.title = title
-Book.prototype.readingStatus = readingStatus
-
+function addBooks(title, auther, pages, readingStatus) {
+    return new Book(title, auther, pages, readingStatus)
 }
+const s1 = addBooks('som1', 'sami', 2221, true)
+const s2 = addBooks('som2', 'sami', 2222, true)
+const s3 = addBooks('som3', 'sami', 2223, false)
+const s4 = addBooks('som3', 'sami', 2224, true)
+
+let myBooks = [s1, s2, s3, s4];
+myBooks.forEach((element) => {
+    const cards = document.querySelector('.cards')
+
+    const card = document.createElement('div')
+    card.className = 'card'
+
+    const title = document.createElement('div')
+    title.className = 'card-book-title'
+    title.innerText = element.title
+
+    const auther = document.createElement('div')
+    auther.innerText = element.auther
+
+    const pages = document.createElement('div')
+    pages.className = 'pages'
+    pages.innerText = element.pages + ' pages'
+
+    const readingStatus = document.createElement('button')
+    readingStatus.className = element.readingStatus == true ? 'reading-status-true' : 'reading-status-false'
+    readingStatus.addEventListener('click', () => {
+        if (element.readingStatus) {
+            element.className = 'reading-status-false'
+        } else
+            element.className = 'reading-status-true'
+    })
+
+    const remove = document.createElement('button')
+    remove.className = 'remove-button'
+    remove.addEventListener('click', () => {
+        
+    })
+
+
+    card.appendChild(title)
+    card.appendChild(auther)
+    card.appendChild(pages)
+    card.appendChild(readingStatus)
+    card.appendChild(remove)
+    cards.appendChild(card)
+})
 
 
 
@@ -31,16 +76,36 @@ Book.prototype.readingStatus = readingStatus
 
 
 
+/*
+const cards = document.querySelector('.cards')
+
+const card = document.createElement('div')
+card.className = 'card'
+
+const title = document.createElement('div')
+title.className = 'card-book-title'
+
+const auther =  document.createElement('div')
+
+const pages = document.createElement('div')
+pages.className = 'pages'
+
+const readingStatus = document.createElement('button')
+readingStatus.className = 'reading-status-button'
+
+const remove = document.createElement('button')
+remove.className = 'remove-button'
+
+card.appendChild(title)
+card.appendChild(auther)
+card.appendChild(pages)
+card.appendChild(readingStatus)
+card.appendChild(remove)
+cards.appendChild(card) */
 
 
 
-
-
-
-
-
-
-
+// myBooks.push(new Book(title, auther, pages, readingStatus))
 
 // function Book(title, auther, page, read) {
 //     this.title = title
